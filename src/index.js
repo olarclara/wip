9c13aa02465a20c69359fd2329f1ebf7758f5078
Maria Clara <claarasantana@gmail.com>
2018-04-09 08:43:47 -0300

    create a POST route

--- src/index.js
+++ src/index.js
@@ -5,6 +5,15 @@ require('dotenv').config();
 const app = express();
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send('Hello, world!'));
+app.get('/', (req, res, next) => {
+  res.send('Hello, world!')
+  next();
+});
+
+app.post('/pr-release', (req, res, next) => {
+  console.log(req.body);
+  res.send();
+  next();
+});
 
 app.listen(5000, () => console.log('Listening on port 5000...'));
