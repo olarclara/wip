24f20c86fadd96abc4b2723b52eb622e2fd3c266
Maria Clara <claarasantana@gmail.com>
2018-04-09 09:06:07 -0300

    config slack client

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { WebClient } = require('@slack/client');
 require('dotenv').config();
 
 const app = express();
 app.use(bodyParser.json());
+
+const web = new WebClient(process.env.SLACK_TOKEN);
+const channelId = 'CA3HATELE';
 
 app.get('/', (req, res, next) => {
   res.send('Hello, world!')
@@ -11,8 +15,13 @@ app.get('/', (req, res, next) => {
 });
 
 app.post('/pr-release', (req, res, next) => {
-  console.log(req.body);
-  res.send();
+  web.chat.postMessage({
+    channel: channelId,
+    text: 'Hello there',
+  })
+  .then(res => console.log('Message sent: ', res.ts))
+  .catch(console.error);
+  res.end();
   next();
 });
 
